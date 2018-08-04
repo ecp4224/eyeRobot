114473751aefb98a5521ccbca816df9b256d368e
Rachel Palmer <palmerr@wit.edu>
2018-08-03 20:18:46 -0400

    Added comments
    Commented RobotDQN and gym_test

--- modules/eyerobot_openai/gym_test.py
+++ modules/eyerobot_openai/gym_test.py
@@ -1,5 +1,4 @@
 import sys
-
 import gym
 
 from eyerobot_gym.config import OPEN_CL, DEBUG, BATCH_SIZE, MEMORY_SIZE, EPOCH
@@ -21,7 +20,6 @@ def blockPrint():
     global devnull
     sys.stdout = devnull
 
-
 # Restore
 def enablePrint():
     sys.stdout = sys.__stdout__
@@ -32,86 +30,90 @@ def action_to_letter(a):
     return actions[a]
 
 
-robot = RobotDQN(load_from="s.ai",
-                 batch_size=BATCH_SIZE,
-                 memory_size=MEMORY_SIZE,
-                 epochs=EPOCH)
-env = gym.make('eyerobot-gym-v0')
+if __name__ == "__main__":
+    # Create model object which represents the AI
+    robot = RobotDQN(load_from="s.ai",
+                     batch_size=BATCH_SIZE,
+                     memory_size=MEMORY_SIZE,
+                     epochs=EPOCH)
+    
+    # Create the environment that the robot will run in
+    env = gym.make('eyerobot-gym-v0')
+    
+    episodes = 1000
+    model_history = None
+    counter = 0
+    for i in range(episodes):
+        
+        state = env.reset()
+        stop = False
+        if counter == 0:
+            while True:
+                r = raw_input("Pick an action:\n\tType r to play the next episode\n\tType g to graph the current model "
+                              "accuraccy\n\tType s to autoplay 100 episodes\n\tType q to safely quit\nAction: ")
+                if r == "q":
+                    stop = True
+                    break
+                elif r == "g" and model_history is not None:
+                    graph(model_history)
+                elif r == "s":
+                    counter = 1000
+                    break
+                else:
+                    stop = False
+                    break
+        else:
+            counter -= 1
+            
+        if stop:
+            break
+        
+        print("Starting episode " + str(i))
+        
+        done = False
+        won = False
 
-episodes = 1000
-model_history = None
-counter = 0
-for i in range(episodes):
+        if not DEBUG:
+            blockPrint()
 
-    state = env.reset()
-    stop = False
-    if counter == 0:
-        while True:
-            r = raw_input("Pick an action:\n\tType r to play the next episode\n\tType g to graph the current model "
-                          "accuraccy\n\tType s to autoplay 100 episodes\n\tType q to safely quit\nAction: ")
-            if r == "q":
-                stop = True
-                break
-            elif r == "g" and model_history is not None:
-                graph(model_history)
-            elif r == "s":
-                counter = 1000
-                break
-            else:
-                stop = False
-                break
-    else:
-        counter -= 1
+        # while game still in progress
+        while not done:
 
-    if stop:
-        break
+            print("What to do..")
+            action = robot.step(state)
 
-    print("Starting episode " + str(i))
+            print("Pressing " + action_to_letter(action))
+            new_state, reward, done, data = env.step(action)
+            
+            print("Remembering..")
+            # Robot automatically remembers the action it just chose
+            robot.save(reward, new_state, done)
+            
+            state = new_state
 
-    done = False
-    won = False
+            # For printing
+            won = reward == 100
 
-    if not DEBUG:
-        blockPrint()
-
-    # while game still in progress
-    while not done:
-
-        print("What to do..")
-        action = robot.step(state)
-
-        print("Pressing " + action_to_letter(action))
-        new_state, reward, done, data = env.step(action)
-
-        print("Remembering..")
-        # Robot automatically remembers the action it just chose
-        robot.save(reward, new_state, done)
-
-        state = new_state
-
-        # For printing
-        won = reward == 100
-
-    if not DEBUG:
-        enablePrint()
-    print("Episode completed!")
-    print("Score: " + str(reward))
-
-    if won:
-        print("I won!")
-    else:
-        print("I lost :(")
-
+        if not DEBUG:
+            enablePrint()
+        print("Episode completed!")
+        print("Score: " + str(reward))
+        
+        if won:
+            print("I won!")
+        else:
+            print("I lost :(")
+            
     print("Training..")
 
     # Only ever train the robot after an entire episode
     model_history = robot.train()
-
-print("Exiting..")
-# save model to file
-
-name = raw_input("Enter name for AI: ")
-
-robot.save_model(name + ".ai")
-env.close()
-print("Goodbye")
+    
+    print("Exiting..")
+    # save model to file
+    
+    name = raw_input("Enter name for AI: ")
+    
+    robot.save_model(name + ".ai")
+    env.close()
+    print("Goodbye")
